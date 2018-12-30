24a90a763863b841fe68a991cca1ef24c564c86c
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-12-30 02:21:03 +0100

    working loki

--- outputs/loki/output_loki.js
+++ outputs/loki/output_loki.js
@@ -4,44 +4,6 @@ var abstract_http = require('./abstract_http'),
 
 var recordCache = require('record-cache');
 
-/* Bulk Helper */
-var template = {"streams": [{"labels": "", "entries": [] }]}
-var onStale = function(data){
- 	for (let [key, value] of data.records.entries()) {
-	     var line = JSON.parse(JSON.stringify(template));
-	     line.streams[0].labels = key;
- 	     value.list.forEach(function(row){
-		// add to array
-		line.streams[0].entries.push({ "ts": row['@timestamp']||new Date().toISOString(), "line": row  });
-             });
-
- 	     var path = this.replaceByFields(data, this.path);
- 	 	if (path) {
- 	 	  var http_options = {
- 	 	    port: this.port,
- 	 	    path: path,
- 	 	    method: 'POST',
- 	 	    headers: {
- 	 	      'Content-Type': 'application/json'
- 	 	    }
- 	 	  };
- 	 	  var line = this.serialize_data(data) || JSON.stringify(data);
- 	 	  if (line) {
- 	 	    http_options.headers['Content-Length'] = Buffer.byteLength(line, 'utf-8');
- 	 	    if ( typeof this.host !== 'string' ) {
- 	 	      for (var i = 0, len = this.host.length; i < len; i++){
- 	 	         http_options.host = this.host[i];
- 	 	         this.sendHttpRequest(http_options, line);
- 	 	      }
- 	 	    } else {
- 	 	         http_options.host = this.host;
- 	 	         this.sendHttpRequest(http_options, line);
- 	 	    }
- 	 	  }
- 	 	}
-        }
-}
-
 var cache;
 
 function LokiPost() {
@@ -55,23 +17,67 @@ function LokiPost() {
       'maxSize': 5000,
       'maxAge': 1000,
     },
+    start_hook: this.start,
   });
+}
+
+LokiPost.prototype.start = function(callback) {
+  /* Bulk Helper */
+  this.onStale = function(data){
+        for (let [key, value] of data.records.entries()) {
+             if(!value.list[0]) return;
+             var line = {"streams": [{"labels": "", "entries": [] }]};
+             line.streams[0].labels="__filename__=\""+key+"\""
+             value.list.forEach(function(row){
+                // add to array
+                row = row.record;
+                line.streams[0].entries.push({ "ts": row['@timestamp']||new Date().toISOString(), "line": row.message  });
+             });
+             line = JSON.stringify(line);
+             var path = this.replaceByFields(data, this.path);
+                if (path) {
+                  var http_options = {
+                    port: this.port,
+                    path: path,
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json'
+                    }
+                  };
+                  if (line) {
+                    http_options.headers['Content-Length'] = Buffer.byteLength(line, 'utf-8');
+                    if ( typeof this.host !== 'string' ) {
+                      for (var i = 0, len = this.host.length; i < len; i++){
+                         http_options.host = this.host[i];
+                         this.sendHttpRequest(http_options, line);
+                      }
+                    } else {
+                         http_options.host = this.host;
+                         this.sendHttpRequest(http_options, line);
+                    }
+                  }
+                }
+        }
+  }.bind(this)
+
   this.cache = recordCache({
-	  maxSize: this.maxSize,
-	  maxAge: this.maxAge,
-	  onStale: onStale
+          maxSize: this.maxSize,
+          maxAge: this.maxAge,
+          onStale: this.onStale
   })
   cache = this.cache;
-}
+
+  callback();
+};
 
 util.inherits(LokiPost, abstract_http.AbstractHttp);
 
 LokiPost.prototype.process = function(data) {
-	cache.add(data);
+        cache.add(data.path,data);
 };
 
 LokiPost.prototype.to = function() {
-  return ' http ' + this.host + ':' + this.port;
+  return ' LOKI http ' + this.host + ':' + this.port + '' + this.path;
 };
 
 exports.create = function() {
