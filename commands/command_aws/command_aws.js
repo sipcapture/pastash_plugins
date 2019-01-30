087eac5b55b17cadbadceab3f5740c788905feac
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2019-01-30 12:16:31 +0100

    test promises

--- commands/command_aws/command_aws.js
+++ commands/command_aws/command_aws.js
@@ -3,55 +3,81 @@
  * (C) 2019 QXIP BV
  */
 
-async function asyncForEach(array, callback) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
+const process = async function(set){
+        if(!set.provider|!set.accessKeyId|!set.secretAccessKey|!set.region|!set.remoteFilename) {
+              set.error = true;
+              return set;
+        } else {
+
+          if(!set.localFilename){
+            set.localFilename = set.remoteFilename;
+          }
+
+          if(!set.downloadedFilePath){
+            set.downloadedFilePath = downloadedFilePath;
+          }
+
+          var aws = require('aws-s3-promisified')({
+            accessKeyId: set.accessKeyId,
+            secretAccessKey: set.secretAccessKey
+          });
+
+          let bucketName = set.id || 'fetchBucket'
+          var res = await aws.saveObjectToFile(bucketName, set.localFilename, set.downloadedFilePath);
+          if (!res) { set.error = true; } else 
+	  { set.error = false; set.localFilename = set.downloadedFilePath+"/"+set.localFilename }
+          return set;
+        }
 }
 
 module.exports = function plugin() {
 
+  this.s3Fetch =  function(data) {
+     var test = Promise.all( data.map( item => item = process(item) ));
+     this.data(data);
+     return this;
+  }
+
+
  this.awsFetch = function(data) {
    let downloadedFilePath = '/tmp'
     if(!Array.isArray(data)) { data = [data]; }
-
 
    const start = async () => {
       await asyncForEach(data, async (set) => {
 
 	if(!set.provider|!set.accessKeyId|!set.secretAccessKey|!set.region|!set.remoteFilename) {
 	      set.error = true;
-	      return;
-      	}
-      	if(!set.localFilename){
-      	    set.localFilename = set.remoteFilename;
-      	}
-	      
-	if(!set.downloadedFilePath){
-      	    set.downloadedFilePath = downloadedFilePath;
-      	}
+      	} else {
 
-	var aws = require('aws-s3-promisified')({
+       	  if(!set.localFilename){
+      	    set.localFilename = set.remoteFilename;
+      	  }
+
+	  if(!set.downloadedFilePath){
+      	    set.downloadedFilePath = downloadedFilePath;
+      	  }
+
+	  var aws = require('aws-s3-promisified')({
 	    accessKeyId: set.accessKeyId,
 	    secretAccessKey: set.secretAccessKey
-	});
+	  });
 
-      	let bucketName = set.id || 'fetchBucket'
-	var res = aws.saveObjectToFile(bucketName, set.localFilename, set.downloadedFilePath);
-        console.log(res);
-	if (!res) { set.error = true; }
-	else { set.localFilename = set.downloadedFilePath+"/"+set.localFilename}
-
+      	  let bucketName = set.id || 'fetchBucket'
+	  var res = await aws.saveObjectToFile(bucketName, set.localFilename, set.downloadedFilePath);
+	  if (!res) { set.error = true; } else { set.error = false; set.localFilename = set.downloadedFilePath+"/"+set.localFilename}
+        }
      });
-       console.log('Done');
+     return data;
    }
-   start();
-   // Apply Data
-   this.data(data);
-   // Return Chain
-   return this;
+   start().then(function() {
+     // Apply Data
+     this.data(data);
+     // Return Chain
+     return this;
+   }.bind(this));
 
  }
 
-}
 
+}
