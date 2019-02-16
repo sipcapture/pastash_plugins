af0fdacd993140aff79a5c59884e9859a0478eaa
Voicenter <shlomi@voicenter.com>
2019-02-16 23:07:36 +0200

    next callback and put some junk demo ...
    update next callback and put some junk demo so i will fill home

--- commands/command_chain/command_chain.js
+++ commands/command_chain/command_chain.js
@@ -1,7 +1,22 @@
-module.exports = function plugin() {
-  this.chain = function() {
-    // return this to allow chaining on this function
-    return this;
-  }
-}
 
+var CircularJSON = require('circular-json');
+module.exports = function plugin() {
+    // decorate class prototype
+    this.main.chain = function(next) {
+        // calling next  this to allow chaining on this function
+        next();
+    }
+  //Some demo staff nedd to clean there for async test concept 
+    this.main.fooFunc= demoFuncaion
+    this.main.barFunc=demoFuncaion
+    this.main.bazFunc=demoFuncaion
+}
+//Some demo staff nedd to clean there for async test concept 
+var demoFuncaion = function(next) {
+    console.log(CircularJSON.stringify(this.data));
+    this.data[self.fieldResultList].push({Done:true})
+    setTimeout(function() {
+        next(null, arguments.callee.toString().substr(0, 3) + ' value'); // Continue on with 'foo value', 'bar value' etc.
+        console.log(arguments.callee.name)
+    }, Math.random() * 3000); // After a random wait of up to a second
+};
