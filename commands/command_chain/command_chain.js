855c7a1c0f511077020794ddda073a2bd69bb22c
Wawezz <tikobaghdasaryan9@gmail.com>
2019-02-28 13:26:07 +0300

    plugins result fixed

--- commands/command_chain/command_chain.js
+++ commands/command_chain/command_chain.js
@@ -20,22 +20,10 @@ module.exports = function plugin(userConf) {
 }
 //Some demo staff nedd to clean there for async test concept 
 var demoFuncaion = function (next) {
-    let result = {
-        PluginName,
-        FunctionName: arguments.callee.name,
-        Index: this.data[self.fieldResultList].length,
-        StartTime: (new Date).getTime(),
-    };
-
     console.log(CircularJSON.stringify(this.data));
 
     setTimeout(function () {
-
-        result.Duration = (new Date).getTime() - result.StartTime;
-        result.ErrorCode = 0;
-
-        this.data[self.fieldResultList].push(result);
-
+        this.data[self.fieldResultList].push({ PluginName: PluginName });
         next(null, arguments.callee.toString().substr(0, 3) + ' value'); // Continue on with 'foo value', 'bar value' etc.
         console.log(arguments.callee.name)
     }.bind(this), Math.random() * conf.timeout); // After a random wait of up to a second
--- commands/command_cryptofile/command_cryptofile.js
+++ commands/command_cryptofile/command_cryptofile.js
@@ -17,36 +17,19 @@ module.exports = function plugin(userConf) {
   };
 
   this.main.encryptFile = function encryptFile(next) {
-    let result = {
-      PluginName,
-      FunctionName: arguments.callee.name,
-      Index: this.data[self.fieldResultList].length,
-      StartTime: (new Date).getTime(),
-    };
 
     encryptor.encryptFile(this.data[conf.inputFileField], this.data[conf.outputFileField], this.data[conf.keyField], options, function (err) {
-      result.Duration = (new Date).getTime() - result.StartTime;
-      result.ErrorCode = err ? err.errno : 0;
-
-      this.data[self.fieldResultList].push(result);
+      this.data[self.fieldResultList].push({ PluginName: PluginName });
       next();
 
     }.bind(this));
   }
 
   this.main.decryptFile = function decryptFile(next) {
-    let result = {
-      PluginName,
-      FunctionName: arguments.callee.name,
-      Index: this.data[self.fieldResultList].length,
-      StartTime: (new Date).getTime(),
-    };
 
     encryptor.decryptFile(this.data[conf.inputFileField], this.data[conf.outputFileField], this.data[conf.keyField], options, function (err) {
-      result.Duration = (new Date).getTime() - result.StartTime;
-      result.ErrorCode = err ? err.errno : 0;
 
-      this.data[self.fieldResultList].push(result);
+      this.data[self.fieldResultList].push({ PluginName: PluginName });
       next();
 
     }.bind(this));
